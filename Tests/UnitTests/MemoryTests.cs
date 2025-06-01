1aad34f87c4d753f694e05a8c11cc59215a40c5a
Dao Hai Nam <daohainam@gmail.com>
2025-06-01 19:16:37 +0200

    Add MemoryTests for RAM and cartridge memory functionality

---
+++ Tests/UnitTests/MemoryTests.cs
@@ -0,0 +1,51 @@
+﻿using mini_6502.Components;
+
+namespace UnitTests;
+public class MemoryTests
+{
+    [Fact]
+    public void ReadWriteMemory_MainRam_Test()
+    {
+        // Arrange
+        var ppu = new Ppu();
+        var memory = new Memory(ppu);
+        byte salt = 0x42;
+
+        // Act
+        for (ushort i = 0; i < 2048; i++)
+        {
+            memory.Write(i, (byte)((i + salt) % 256));
+        }
+
+
+        // Read back the value
+        for (ushort i = 0; i < 2048; i++)
+        {
+            byte valueRead = memory.Read(i);
+            Assert.Equal((byte)((i + salt) % 256), valueRead);
+        }
+    }
+
+    [Fact]
+    public void ReadWriteMemory_Cartridge_Test()
+    {
+        // Arrange
+        var ppu = new Ppu();
+        var memory = new Memory(ppu);
+        byte salt = 0x77;
+
+        // Act
+        for (ushort i = 0; i < 0x8000; i++)
+        {
+            memory.Write((ushort)(i + 0x8000), (byte)((i + salt) % 256));
+        }
+
+
+        // Read back the value
+        for (ushort i = 0; i < 0x8000; i++)
+        {
+            byte valueRead = memory.Read((ushort)(i + 0x8000));
+            Assert.Equal((byte)((i + salt) % 256), valueRead);
+        }
+    }
+}
