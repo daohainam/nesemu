77bec20c0b43e541a251b5eb8ae490544ba9ea81
daohainam <daohainam@gmail.com>
2025-06-03 10:23:29 +0200

    Refator: move instructions to separate classes

--- src/mini-6502/InstructionSet.cs
+++ src/mini-6502/InstructionSet.cs
@@ -1,4 +1,5 @@
 ﻿using mini_6502.Components;
+using mini_6502.Instructions;
 
 namespace mini_6502;
 internal partial class InstructionSet
@@ -18,51 +19,14 @@ internal partial class InstructionSet
         }
 
         // Load/Store Instructions
-        instructions[0xA9] = new Instruction("LDA", AddressingMode.Immediate, 2, 2, OpLDA);
-        instructions[0xA5] = new Instruction("LDA", AddressingMode.ZeroPage, 3, 2, OpLDA);
-        instructions[0xB5] = new Instruction("LDA", AddressingMode.ZeroPageX, 4, 2, OpLDA);
-        instructions[0xAD] = new Instruction("LDA", AddressingMode.Absolute, 4, 3, OpLDA);
-        instructions[0xBD] = new Instruction("LDA", AddressingMode.AbsoluteX, 4, 3, OpLDA);
-        instructions[0xB9] = new Instruction("LDA", AddressingMode.AbsoluteY, 4, 3, OpLDA);
-        instructions[0xA1] = new Instruction("LDA", AddressingMode.IndirectX, 6, 2, OpLDA);
-        instructions[0xB1] = new Instruction("LDA", AddressingMode.IndirectY, 5, 2, OpLDA);
-    }
-
-    private static byte ReadMemory(Cpu cpu, IMemory memory, AddressingMode mode)
-    {
-        switch (mode)
-        {
-            case AddressingMode.Immediate:
-                return memory.Read(cpu.PC++);
-            case AddressingMode.ZeroPage:
-                return memory.Read(memory.Read(cpu.PC++));
-            case AddressingMode.ZeroPageX:
-                return memory.Read((byte)(memory.Read(cpu.PC++) + cpu.X));
-            case AddressingMode.Absolute:
-                ushort address = (ushort)(memory.Read(cpu.PC++) | (memory.Read(cpu.PC++) << 8));
-                return memory.Read(address);
-            case AddressingMode.AbsoluteX:
-                address = (ushort)(memory.Read(cpu.PC++) | (memory.Read(cpu.PC++) << 8));
-                return memory.Read((ushort)(address + cpu.X));
-            case AddressingMode.AbsoluteY:
-                address = (ushort)(memory.Read(cpu.PC++) | (memory.Read(cpu.PC++) << 8));
-                return memory.Read((ushort)(address + cpu.Y));
-            case AddressingMode.IndirectX:
-                byte zpAddress = memory.Read(cpu.PC++);
-                ushort indirectAddress = (ushort)(zpAddress + cpu.X);
-                ushort addressLow = memory.Read(indirectAddress);
-                ushort addressHigh = memory.Read((ushort)((indirectAddress + 1) & 0xFF)); // Wrap around for zero page
-                return memory.Read((ushort)(addressHigh << 8 | addressLow));
-            case AddressingMode.IndirectY:
-                zpAddress = memory.Read(cpu.PC++);
-                indirectAddress = (ushort)(zpAddress + cpu.Y);
-                addressLow = memory.Read(indirectAddress);
-                addressHigh = memory.Read((ushort)((indirectAddress + 1) & 0xFF)); // Wrap around for zero page
-                return memory.Read((ushort)(addressHigh << 8 | addressLow));
-            default:
-                Cpu.Panic($"Unsupported addressing mode: {mode}");
-                return 0; // Unreachable, but required to satisfy the compiler
-        }
+        instructions[0xA9] = new Instruction("LDA", AddressingMode.Immediate, 2, 2, InstructionSet_LoadStore.OpLDA);
+        instructions[0xA5] = new Instruction("LDA", AddressingMode.ZeroPage, 3, 2, InstructionSet_LoadStore.OpLDA);
+        instructions[0xB5] = new Instruction("LDA", AddressingMode.ZeroPageX, 4, 2, InstructionSet_LoadStore.OpLDA);
+        instructions[0xAD] = new Instruction("LDA", AddressingMode.Absolute, 4, 3, InstructionSet_LoadStore.OpLDA);
+        instructions[0xBD] = new Instruction("LDA", AddressingMode.AbsoluteX, 4, 3, InstructionSet_LoadStore.OpLDA);
+        instructions[0xB9] = new Instruction("LDA", AddressingMode.AbsoluteY, 4, 3, InstructionSet_LoadStore.OpLDA);
+        instructions[0xA1] = new Instruction("LDA", AddressingMode.IndirectX, 6, 2, InstructionSet_LoadStore.OpLDA);
+        instructions[0xB1] = new Instruction("LDA", AddressingMode.IndirectY, 5, 2, InstructionSet_LoadStore.OpLDA);
     }
 
     private class InvalidOpcodeInstruction(int index) : Instruction("INVALID", AddressingMode.Implied, 1, 1,
